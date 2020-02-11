dc28a601e177d4b179b9049a124cda4f87913703
abiisnn <abigail.nic.say@hotmail.com>
2020-02-11 16:35:05 -0600

    Class

--- Notes/classe/Normalize.py
+++ Notes/classe/Normalize.py
@@ -1,5 +1,6 @@
 import nltk
 import re
+import math
 from bs4 import BeautifulSoup
 from nltk.corpus import PlaintextCorpusReader
 from nltk.corpus import stopwords
@@ -80,17 +81,132 @@ def initializeContext(tokens, vocabulary):
 
 	return contexto
 
+def leftContextPosition(pos, window):
+	pos = pos - window
+	if(pos < 0):
+		pos = 0
+	return pos
+
+def rightContextPosition(pos, window, n):
+	pos = pos + window
+	if(pos > n):
+		pos = n - 1 
+	return pos
+
+#Parameters: string: 
+#Return: 
+def getContext(token, positions, window, originalText):
+	context = []
+	if token in positions:
+		for pos in positions[token]:
+			lpos = leftContextPosition(pos, window)
+			rpos = rightContextPosition(pos, window, len(originalText))
+			#con = []
+			for i in range(lpos, pos):
+				context.append(originalText[i])
+			#con.append(token)
+			for i in range(pos + 1, rpos):
+				context.append(originalText[i])			
+			#context.append(con)
+	return context
+
+def pointProduct(a, b):
+	ans = 0
+	for i in range(0, len(a)):
+		ans += (a[i] * b[i])
+	return ans
+
+def printContext(context):
+	for i in range(0, len(context)):
+		aux = ''
+		for j in range(0, len(context[i])):
+			aux += context[i][j] + " "
+		print(aux)
+
+def mag(v):
+	ans = 0
+	for i in range(0, len(v)):
+		ans += (v[i] * v[i])
+	return math.sqrt(ans)
+
+def createFileDic(path, dic):
+	f = open(path, 'w')
+	for key, value in dic.items():
+		f.write(key + " ")
+		f.write(str(value))
+		f.write('\n')
+	f.close()
+ #    print (key, value)
+	# for word in dic:
+	# 	aux = dic[word]
+	# 	f.write(word + " ")
+	# 	f.write(aux)
+	# 	f.write('\n')
+	
+	# f.close()
+
 fpath = '/Users/27AGO2019/Desktop/AbiiSnn/GitHub/Natural-Language-Processing/corpus/e961024.htm'
-nameFile = '/Users/27AGO2019/Desktop/AbiiSnn/GitHub/Natural-Language-Processing/Normalize/e961024.txt'
+nameFile = '/Users/27AGO2019/Desktop/AbiiSnn/GitHub/Natural-Language-Processing/Normalize/similitud.txt'
 language = 'spanish'
 code = 'utf-8'
 textSource = getText(fpath, code)
 tokensHtml = getTokens(textSource)
 cleanTokens = getCleanTokens(tokensHtml)
+
 tokens = removeStopwords(cleanTokens, language)
 vocabulary = getVocabulary(tokens)
 
-positions = initializeContext(tokens, vocabulary) 
+positions = initializeContext(tokens, vocabulary) #Initialize Context
 
+#Get contexts
+contexts = {}
+print("Context:")
+for term in vocabulary:
+	contexts[term] = getContext(term, positions, 8, tokens)
+	print(contexts[term])
 
-createFile(nameFile, vocabulary)
+#Get frecuency
+vectors = {}
+for term in vocabulary:
+	context = contexts[term]
+	vector = []
+	for t in vocabulary:
+		frec = context.count(t)
+		vector.append(frec)
+	vectors[term] = vector
+
+#Get List
+similitud = {}
+word = "grande"
+v = vectors[word]
+for term in vocabulary:
+	vec = vectors[term]
+	if mag(v) != 0 and mag(vec) != 0:
+		cos = pointProduct(v, vec) / (mag(v) * mag(vec))
+		similitud[term] = cos
+{k: v for k, v in sorted(similitud.items(), key=lambda item: item[1])}
+
+# for i in sorted(similitud):
+# 	print((i, similitud[i]), end = " ")
+
+createFileDic(nameFile, similitud)
+
+'''
+For each term, get context
+
+contexts[term] = list of context
+vectors[term] = num vector
+
+For each term, get context
+	context = contexts[term]
+	vector = []
+	for each term 
+		frec = contex.count(term)
+	vector.append(frec)
+
+vector = vectors[word]
+for each ter
+	vec = vectors[term]
+	coseno = 
+'''
+# createFile(nameFile, vocabulary)
